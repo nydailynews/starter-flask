7197a50ceb77313ace44c5eb5d6e99389c553258
freejoe76 <jmurphy@denverpost.com>
2017-06-22 17:42:02 -0400

    Add the particular deploy scripts we use here

--- flask/deploy.bash
+++ flask/deploy.bash
@@ -1,3 +1,49 @@
 #!/bin/bash
-source source.bash
-python app.py --name 2016
+# scp flat files to prod.
+ENVIRON='PROD'
+export environ=$ENVIRON
+SERVER='qa'
+DIR='/path/to/project/root/'
+PROJECT='projectname'
+LOCATION_OVERRIDE=0
+html_only() { 
+    echo ''
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -e | --environ ) shift
+            ENVIRON=$1
+            ;;
+        -p | --project ) shift
+            PROJECT=$1
+            ;;
+        -s | --server ) shift
+            SERVER=$1
+            ;;
+        -d | --dir ) shift
+            DIR=$1
+            ;;
+        -h | --htmlonly ) shift
+            html_only() {
+                find application/public \! -name "*.html" -type f -delete;
+            }
+            ;;
+        -f | --freeze ) shift
+            python freeze.py; export environ='DEV'; exit 1
+            ;;
+    esac
+    shift
+done
+
+# If we bailed on deploy mid-deploy we'll still have the public dir around.
+if [ -d "application/$PROJECT" ]; then rm -fr application/$PROJECT; fi
+
+#python tests.py && \
+html_only && \
+    python freeze.py && \
+    mv application/build application/$PROJECT && \
+    scp -r application/$PROJECT $SERVER:$DIR && \
+    mv application/$PROJECT application/build
+
+export environ='DEV'
---
+++ flask/freeze.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# More here: http://pythonhosted.org/Frozen-Flask/
+import argparse
+import sys
+import os
+import doctest
+from flask_frozen import Freezer
+from application import app
+
+app.debug = False
+environ = os.getenv('environ', 'DEV')
+if environ == 'PROD':
+    app.url_root = '/path/to/project/root/'
+
+class FreezeThings:
+    """ We put the Flask Frozen methods in this class so we don't have to
+        freeze everything at once.
+        >>> f = FreezeThings()
+        >>> files_frozen = f.freezer.freeze()
+        """
+
+    def __init__(self):
+        """
+            >>> f = FreezeThings()
+            """
+        self.freezer = Freezer(app)
+        self.chapters_all = app.chapters
+
+    def freeze_urls(self, path):
+        """ Freeze one url (if passed a path) or a handful (if passed a list
+            of paths).
+            >>> f = FreezeThings()
+            >>> f.freeze_urls('/chapter/01/')
+            """
+        @self.freezer.register_generator
+        def freeze_urls():
+            return path
+
+def main(args):
+    """ What we run when we run this from the command line.
+        >>> main(build_parser(None))
+        """
+    f = FreezeThings()
+
+    if args.paths[0]:
+        f.freeze_urls(args.paths[0])
+        f.freezer.freeze()
+        return True
+
+    f.freezer.freeze()
+
+def build_parser(args):
+    """ A method to handle argparse.
+        >>> args = build_parser(None)
+        >>> print args.verbose
+        True
+        """
+    parser = argparse.ArgumentParser(usage='$ python freeze.py',
+                                     description='''Turn the flask app into a
+                                                    bunch of html files.''',
+                                     epilog='')
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
+    parser.add_argument(dest="paths", action="append", nargs="*")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = build_parser(sys.argv)
+    if args.verbose == True:
+        doctest.testmod(verbose=True)
+    main(args)
